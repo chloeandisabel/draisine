536059ee6d4d99004095bb4be4decb36ab0fc26a
Mark Abramov <markizko@gmail.com>
2016-01-14 17:42:34 +0300

    Update salesforce_equals? function to compare JSON-formatted timestamps as time

--- lib/draisine/util/salesforce_comparisons.rb
+++ lib/draisine/util/salesforce_comparisons.rb
@@ -1,5 +1,6 @@
 module Draisine
   module SalesforceComparisons
+    JSON_TIME_REGEX = /\A\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(.\d{3})?[+\-]\d{2}:\d{2}\z/
     module_function
 
     def salesforce_equals?(value, other)
@@ -16,7 +17,8 @@ module Draisine
     end
 
     def salesforce_coerce(value)
-      if value.kind_of?(DateTime) || value.kind_of?(Time) || value.kind_of?(Date)
+      if value.kind_of?(DateTime) || value.kind_of?(Time) || value.kind_of?(Date) ||
+         value =~ JSON_TIME_REGEX
         value = value.to_time.utc.change(usec: 0)
       end
       value
