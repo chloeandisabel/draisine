79b6d2dd552de439ed9681b76df8cd151481729e
Mark Abramov <markizko@gmail.com>
2016-01-18 15:09:17 +0300

    Queue into salesforce_job queue

--- lib/draisine/jobs/job_base.rb
+++ lib/draisine/jobs/job_base.rb
@@ -1,5 +1,7 @@
 module Draisine
   class JobBase < ActiveJob::Base
+    queue_as :salesforce_job
+
     rescue_from StandardError do |ex|
       Draisine.job_error_handler.call(ex, self, arguments)
       raise ex
