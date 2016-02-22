eaf4509d3ad96ef932424cba3b1428c05b47be57
Mark Abramov <markizko@gmail.com>
2016-02-23 02:12:13 +0300

    Use the same partitioning mechanism in poller as in auditor

--- lib/draisine/poller.rb
+++ lib/draisine/poller.rb
@@ -2,22 +2,49 @@ module Draisine
   class Poller
     Result = Struct.new(:salesforce_count, :db_count, :created_count, :updated_count, :deleted_count)
 
-    attr_reader :model_class, :mechanism
+    class <<self
+      def run(model_class:, mechanism: :default, start_date:, end_date: Time.current, **run_args)
+        partitions = partition(
+          model_class: model_class,
+          mechanism: mechanism,
+          start_date: start_date,
+          end_date: end_date,
+          partition_size: 10**12)
+        run_partition(partitions.first, **run_args)
+      end
+      alias_method :poll, :run
 
-    def initialize(model_class, mechanism = :default)
-      @model_class = model_class
-      @mechanism = QueryMechanisms.fetch(mechanism).new(model_class)
+      def partition(model_class:, mechanism: :default, start_date:, end_date: Time.current, partition_size: 100)
+        Partitioner.partition(
+          model_class: model_class,
+          mechanism: mechanism,
+          start_date: start_date,
+          end_date: end_date,
+          partition_size: partition_size)
+      end
+
+      def run_partition(partition, **run_args)
+        new(partition).run(**run_args)
+      end
+      alias_method :poll_partition, :run_partition
     end
 
-    def poll(start_date, end_date = Time.now, import_created: true, import_updated: false, import_deleted: true)
+
+    attr_reader :partition, :model_class, :start_date, :end_date
+    def initialize(partition)
+      @partition = partition
+      @model_class = partition.model_class
+      @start_date = partition.start_date
+      @end_date = partition.end_date
+    end
+
+    def run(import_created: true, import_updated: false, import_deleted: true)
       created_count = updated_count = deleted_count = 0
       if import_created || import_updated
-        created_count, updated_count = import_changes(start_date, end_date, import_created, import_updated)
+        created_count, updated_count = import_changes(import_created, import_updated)
       end
 
-      if import_deleted
-        deleted_count = import_deletes(start_date, end_date)
-      end
+      deleted_count = import_deletes if import_deleted
 
       Result.new(
         client.count(salesforce_object_name),
@@ -26,15 +53,21 @@ module Draisine
         updated_count,
         deleted_count)
     end
+    alias_method :poll, :run
 
     protected
 
-    def import_changes(start_date, end_date, import_created, import_updated)
+    def import_changes(import_created, import_updated)
+      updated_ids = partition.updated_ids
+      return [0, 0] unless updated_ids.present?
+
       created_count = updated_count = 0
-      changed_ids = mechanism.get_updated_ids(start_date, end_date)
-      changed_objects = client.fetch_multiple(salesforce_object_name, changed_ids)
-      existing_models = model_class.where(salesforce_id: changed_ids)
-        .each_with_object({}) {|model, rs| rs[model.salesforce_id] = model }
+      changed_objects = client.fetch_multiple(salesforce_object_name, updated_ids)
+
+      existing_models = model_class
+        .where(salesforce_id: updated_ids)
+        .each_with_object({}) { |model, rs| rs[model.salesforce_id] = model }
+
       changed_objects.each do |object|
         id = object.attributes.fetch('Id')
         model = existing_models[id]
@@ -53,8 +86,8 @@ module Draisine
       [created_count, updated_count]
     end
 
-    def import_deletes(start_date, end_date)
-      deleted_ids = mechanism.get_deleted_ids(start_date, end_date)
+    def import_deletes
+      deleted_ids = partition.deleted_ids
       return 0 unless deleted_ids.present?
       model_class.where(salesforce_id: deleted_ids).delete_all
     end
--- spec/draisine/poller_spec.rb
+++ spec/draisine/poller_spec.rb
@@ -3,23 +3,21 @@ require "spec_helper_ar"
 describe Draisine::Poller do
   include_context "Salesforce stubs"
 
-  subject { described_class.new(model_class) }
+  subject { described_class }
+
   let(:model_class) { Lead }
   let(:start_date) { 10.minutes.ago }
   let(:end_date) { Time.now }
 
   before do
     salesforce_stub_out_leads!
+    allow(sf_client).to receive(:get_updated_ids).and_return([])
+    allow(sf_client).to receive(:get_deleted_ids).and_return([])
+    allow(sf_client).to receive(:fetch_multiple).and_return([])
+    allow(sf_client).to receive(:count).and_return(0)
   end
 
-  describe "#poll" do
-    before do
-      allow(sf_client).to receive(:get_updated_ids).and_return([])
-      allow(sf_client).to receive(:get_deleted_ids).and_return([])
-      allow(sf_client).to receive(:fetch_multiple).and_return([])
-      allow(sf_client).to receive(:count).and_return(0)
-    end
-
+  describe ".poll" do
     it "imports new records" do
       allow(sf_client).to receive(:get_updated_ids).and_return(["A001"])
       allow(sf_client).to receive(:fetch_multiple).and_return([
@@ -27,7 +25,7 @@ describe Draisine::Poller do
       ])
 
       expect {
-        subject.poll(start_date, end_date, import_created: true)
+        subject.poll(model_class: model_class, start_date: start_date, end_date: end_date, import_created: true)
       }.to change { model_class.count }.by(1)
       lead = model_class.last
       expect(lead.FirstName).to eq("John")
@@ -42,7 +40,7 @@ describe Draisine::Poller do
       ])
 
       expect {
-        subject.poll(start_date, end_date, import_updated: true)
+        subject.poll(model_class: model_class, start_date: start_date, end_date: end_date, import_updated: true)
       }.not_to change { model_class.count }
       lead.reload
       expect(lead.FirstName).to eq("John")
@@ -56,7 +54,7 @@ describe Draisine::Poller do
       ])
 
       expect {
-        subject.poll(start_date, end_date, import_updated: false)
+        subject.poll(model_class: model_class, start_date: start_date, end_date: end_date, import_updated: false)
       }.not_to change { model_class.count }
       lead.reload
       expect(lead.FirstName).to eq("Cole")
@@ -66,7 +64,7 @@ describe Draisine::Poller do
       lead = model_class.create_without_callbacks!(salesforce_id: "A001")
       allow(sf_client).to receive(:get_deleted_ids).and_return(["A001"])
       expect {
-        subject.poll(start_date, end_date, import_deleted: true)
+        subject.poll(model_class: model_class, start_date: start_date, end_date: end_date, import_deleted: true)
       }.to change { model_class.count }.by(-1)
     end
 
@@ -74,19 +72,18 @@ describe Draisine::Poller do
       lead = model_class.create_without_callbacks!(salesforce_id: "A001")
       allow(sf_client).to receive(:get_deleted_ids).and_return(["A001"])
       expect {
-        subject.poll(start_date, end_date, import_deleted: false)
+        subject.poll(model_class: model_class, start_date: start_date, end_date: end_date, import_deleted: false)
       }.to change { model_class.count }.by(0)
     end
 
     it "supports a different system modstamp polling mechanism" do
-      subject = described_class.new(model_class, :system_modstamp)
       expect(sf_client).to receive(:query).with(/SELECT Id FROM/).and_return([
         MaterializedModelInstance.new("Id" => "A000")
       ])
       expect(sf_client).to receive(:fetch_multiple).and_return([
         MaterializedModelInstance.new("Id" => "A000", "FirstName" => "John")
       ])
-      expect { subject.poll(start_date, end_date) }.to change { model_class.count }.by(1)
+      expect { subject.poll(model_class: model_class, start_date: start_date, end_date: end_date, mechanism: :system_modstamp) }.to change { model_class.count }.by(1)
     end
 
     it "doesn't update the record if salesforce SystemModstamp is older than current SystemModstamp" do
@@ -95,7 +92,7 @@ describe Draisine::Poller do
       allow(sf_client).to receive(:fetch_multiple).and_return([
         MaterializedModelInstance.new("FirstName" => "John", "Id" => "A001", "SystemModstamp" => 3.days.ago)
       ])
-      subject.poll(start_date, end_date, import_updated: true)
+      subject.poll(model_class: model_class, start_date: start_date, end_date: end_date, import_updated: true)
       expect(lead.reload.FirstName).to eq("Cole")
     end
 
@@ -107,7 +104,7 @@ describe Draisine::Poller do
       it "returns salesforce and db counters after running" do
         expect(sf_client).to receive(:count).and_return(42)
         model_class.create_without_callbacks!(salesforce_id: "A001")
-        result = subject.poll(start_date, end_date)
+        result = subject.poll(model_class: model_class, start_date: start_date, end_date: end_date)
         expect(result.salesforce_count).to eq(42)
         expect(result.db_count).to eq(1)
       end
@@ -122,11 +119,22 @@ describe Draisine::Poller do
           MaterializedModelInstance.new("FirstName" => "Jack", "Id" => "A004")
         ])
         allow(sf_client).to receive(:get_deleted_ids).and_return(["A002", "A003"])
-        result = subject.poll(start_date, end_date, import_created: true, import_updated: true, import_deleted: true)
+        result = subject.poll(model_class: model_class, start_date: start_date, end_date: end_date, import_created: true, import_updated: true, import_deleted: true)
         expect(result.created_count).to eq(1)
         expect(result.updated_count).to eq(1)
         expect(result.deleted_count).to eq(2)
       end
+    end
+  end
+
+  describe ".run_partition" do
+    it "allows running a specific partition" do
+      allow(sf_client).to receive(:get_updated_ids).and_return(["A001"])
+      allow(sf_client).to receive(:fetch_multiple).and_return([
+        MaterializedModelInstance.new("FirstName" => "John", "Id" => "A001")
+      ])
+      partition = Draisine::Partition.new(model_class, start_date, end_date, ["A001"])
+      subject.run_partition(partition)
     end
   end
 end
